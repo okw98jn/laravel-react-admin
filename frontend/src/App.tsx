1048976cf6a077d88bd3d5a7fb614e36aa366fd5
okawa <0cu2045d@gmail.com>
2023-10-29 16:36:41 +0900

    ヘッダー変更

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RecoilRoot, } from 'recoil';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
@@ -8,8 +8,10 @@ import "./css/app.scss";
 import "./css/index.css";
 import { CustomSnackbar } from "./Pages/components/CustomSnackbar";
 import { AxiosClientProvider } from "./Axios/AxiosClientProvider";
-
 export const App: React.FC = () => {
+    useEffect(() => {
+        import('preline');
+    }, []);
     return (
         <RecoilRoot>
             <BrowserRouter>
--- frontend/src/Pages/Admin/components/Header.tsx
+++ frontend/src/Pages/Admin/components/Header.tsx
@@ -15,10 +15,24 @@ const Header: React.FC = React.memo(() => {
                     <AiOutlineMenu onClick={() => setIsOpen(!isOpen)} />
                 </IconContext.Provider>
             </div>
-            <div>
-                <p>name</p>
-            </div>
-        </HeaderArea>
+            <nav className="relative max-w-[98rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+                <div id="navbar-collapse-basic" className="hidden basis-full grow sm:block">
+                    <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
+                        <div className="hs-dropdown [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
+                            <button id="hs-mega-menu-basic-dr" type="button" className="flex items-center w-full text-gray-600 hover:text-gray-400 font-medium">
+                                大川心
+                                <svg className="ml-2 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
+                                </svg>
+                            </button>
+                            <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.9ms] sm:duration-[250ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-3 bg-white sm:shadow-md rounded-lg p-2 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
+                                <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100" href="#">ログアウト</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </HeaderArea >
     )
 })
 
