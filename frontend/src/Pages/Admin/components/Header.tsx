3844406047f9ff34cf601a516d42d845885a61d9
okawa <0cu2045d@gmail.com>
2023-10-30 01:20:03 +0900

    ログインユーザー名表示

--- frontend/src/Pages/Admin/components/Header.tsx
+++ frontend/src/Pages/Admin/components/Header.tsx
@@ -7,8 +7,10 @@ import { styled } from 'styled-components'
 import { sidebarState } from '../../../Recoil/Admin/sidebarState';
 import { axiosClient } from '../../../Axios/AxiosClientProvider';
 import { useNavigate } from 'react-router-dom';
+import { useAdminState } from '../../../Recoil/Admin/auth';
 
 const Header: React.FC = React.memo(() => {
+    const { admin }           = useAdminState();
     const [isOpen, setIsOpen] = useRecoilState(sidebarState);
     const navigate            = useNavigate();
     const logoutSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
@@ -32,7 +34,7 @@ const Header: React.FC = React.memo(() => {
                     <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
                         <div className="hs-dropdown [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
                             <button id="hs-mega-menu-basic-dr" type="button" className="flex items-center w-full text-gray-600 hover:text-gray-400 font-medium">
-                                大川心
+                                {admin?.name}
                                 <svg className="ml-2 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
                                 </svg>
