e2425f0d7f871b8280bbd23eddbb01c613ca7f69
okawa <0cu2045d@gmail.com>
2023-10-30 22:51:12 +0900

    ヘッダーレイアウト修正

--- frontend/src/Pages/Admin/components/Header.tsx
+++ frontend/src/Pages/Admin/components/Header.tsx
@@ -10,9 +10,9 @@ import { useNavigate } from 'react-router-dom';
 import { useAdminState } from '../../../Recoil/Admin/auth';
 
 const Header: React.FC = React.memo(() => {
-    const { admin, setAdmin }           = useAdminState();
+    const { admin, setAdmin } = useAdminState();
     const [isOpen, setIsOpen] = useRecoilState(sidebarState);
-    const navigate            = useNavigate();
+    const navigate = useNavigate();
     const logoutSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
         axiosClient.post('/api/admin/logout/')
@@ -30,25 +30,20 @@ const Header: React.FC = React.memo(() => {
                     <AiOutlineMenu onClick={() => setIsOpen(!isOpen)} />
                 </IconContext.Provider>
             </div>
-            <nav className="relative max-w-[98rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-                <div id="navbar-collapse-basic" className="hidden basis-full grow sm:block">
-                    <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
-                        <div className="hs-dropdown [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
-                            <button id="hs-mega-menu-basic-dr" type="button" className="flex items-center w-full text-gray-600 hover:text-gray-400 font-medium">
-                                {admin?.name}
-                                <svg className="ml-2 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
-                                </svg>
-                            </button>
-                            <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.9ms] sm:duration-[250ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-3 bg-white sm:shadow-md rounded-lg p-2 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
-                                <form onSubmit={e => logoutSubmit(e)}>
-                                    <button className="flex items-center gap-x-3.5 py-2 px-3 w-full rounded-md text-sm text-gray-800 hover:bg-gray-100">ログアウト</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+            <div className="relative hs-dropdown [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
+                <button id="hs-mega-menu-basic-dr" type="button" className="flex items-center w-full text-gray-600 hover:text-gray-400 font-medium">
+                    {admin?.name}
+                    <svg className="ml-2 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
+                    </svg>
+                </button>
+                <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.9ms] sm:duration-[250ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-40 hidden z-10 sm:mt-3 bg-white sm:shadow-md rounded-lg p-2 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
+                    <button className="flex items-center gap-x-3.5 py-2 mb-1 px-3 w-full rounded-md text-sm text-gray-800 hover:bg-gray-100">マイページ</button>
+                    <form onSubmit={e => logoutSubmit(e)}>
+                        <button className="flex items-center gap-x-3.5 py-2 mb-1 px-3 w-full rounded-md text-sm text-gray-800 hover:bg-gray-100">ログアウト</button>
+                    </form>
                 </div>
-            </nav>
+            </div>
         </HeaderArea >
     )
 })
@@ -61,7 +56,7 @@ const HeaderArea = styled.header`
     display: flex; 
     align-items: center;
     justify-content: space-between;
-    padding: 0 1%;
+    padding: 0 7% 0 1%;
 `
 
 export default Header
